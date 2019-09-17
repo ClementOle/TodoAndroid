dbcecf47864fc7ffb27d67fe219b731831beefe3
Clément Olewski <clement.olewski@gmail.com>
2019-09-17 16:39:05 +0200

    Ajout des fichiers de base

---
+++ app/src/main/java/com/colewski/todoandroid/Database/TaskDatabase.java
@@ -0,0 +1,4 @@
+package com.colewski.todoandroid.Database;
+
+public class TaskDatabase {
+}
---
+++ app/src/main/java/com/colewski/todoandroid/Model/TaskModel.java
@@ -0,0 +1,7 @@
+package com.colewski.todoandroid.Model;
+
+public class TaskModel {
+    private Integer id;
+    private String content;
+
+}
